e0b1b55844ed5e74f8e1eba8ddd4226c4542fec5
Ian Maher <iformaher@gmail.com>
2025-06-05 20:17:05 -0700

    Fixing base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/your-repo-name/', // 👈 Set this to your repo name
+  base: '/react-map/', // 👈 Set this to your repo name
   plugins: [react()],
 })
